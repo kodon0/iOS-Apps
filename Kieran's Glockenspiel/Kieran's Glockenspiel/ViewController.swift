6f6cfed8c48c53bfe6ad237b890b25147d963169
Kieran O'Donnell <kieran.j.odonnell@gmail.com>
2020-12-30 15:10:14 +1100

    FInished Glock app

--- Kieran's Glockenspiel/Kieran's Glockenspiel/ViewController.swift
+++ Kieran's Glockenspiel/Kieran's Glockenspiel/ViewController.swift
@@ -19,12 +19,25 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        
-        print("I get pressed")
-        playSound()
-    }
-    func playSound() {
-        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        // Gets title of button, prints it with crrentTitle
+
+        print("\(sender.currentTitle) got pressed!")
+                
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.6
+
+        //Code should execute after 0.1 second delay.
+         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+             //Bring's sender's opacity back up to fully opaque.
+             sender.alpha = 1.0
+            
+        // Plays sound based on note
+        self.playSound(note: sender.currentTitle!)
+          }
+        }
+    // Define a note: String
+    func playSound(note: String) {
+        let url = Bundle.main.url(forResource: note, withExtension: "wav")
             player = try! AVAudioPlayer(contentsOf: url!)
             player.play()
     }
