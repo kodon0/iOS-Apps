c50d4f88cfdb4fd2db61a1b91cd2c3110875718d
Kieran O'Donnell <kieran.j.odonnell@gmail.com>
2021-01-05 16:48:56 +1100

    added timer function

--- Kieran's Egg Timer/Kieran's Egg Timer/ViewController.swift
+++ Kieran's Egg Timer/Kieran's Egg Timer/ViewController.swift
@@ -10,28 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
-//    Define time dict
-    let eggTimes = ["Soft": 4, "Medium":8, "Hard": 12]
+    @IBOutlet var eggLabel: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    //    Define time dict
+    let eggTimes = ["Soft": (3), "Medium":(4), "Hard": (5)]
+//    Initial value of counter
+    var counter = 60
+    
+    var timer = Timer()
+//    Removed viewDidLoad - no useful code for this method so safe to delete since any superclass that overrides it will still be called
 
     @IBAction func hardnessSelector(_ sender: UIButton) {
-        let hardness = sender.currentTitle!
         
-        switch hardness {
-        case "Soft":
-            print(eggTimes["Soft"]!)
-        case "Medium":
-            print(eggTimes["Medium"]!)
-        case "Hard":
-            print(eggTimes["Hard"]!)
-        default:
-            print("Time out of range")
-        }
-    }
+//        Invalidate time before starting
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle!
+        counter = eggTimes[hardness]!
     
-}
+//    Needed to close IBAction part!
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+        @objc func updateCounter() {
+            if counter > 0 {
+                print("\(counter) seconds to the end of the world")
+                counter -= 1
+            } else if (counter == 0){
+                timer.invalidate()
+                eggLabel.text = "Poof! Donezo!"
+                }
+        }
+        
+    }
+
 
