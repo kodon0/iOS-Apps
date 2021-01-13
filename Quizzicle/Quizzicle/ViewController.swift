41b83bc50c7cf1cd7b56abf233b761e1be5b3708
Kieran O'Donnell <kieran.j.odonnell@gmail.com>
2021-01-13 16:34:33 +1100

    started quizzicle quizz game

---
+++ Quizzicle/Quizzicle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quizzicle
+//
+//  Created by Kieran O'Donnell on 13/01/2021.
+//  Copyright © 2021 baxmanduppa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
