391f86ea1d210f1daf1fc383b8f135b6d24b4cb4
Kieran O'Donnell <kieran.j.odonnell@gmail.com>
2021-01-22 09:40:00 +1100

    added text field editing

--- Caeli/Caeli/Controller/WeatherViewController.swift
+++ Caeli/Caeli/Controller/WeatherViewController.swift
@@ -8,12 +8,44 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
-
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+    
+//    Need to inclued UITextFieldDelegate to register what happens in text field
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextField.delegate = self
     }
 
+    @IBAction func searchButtonPressed(_ sender: Any) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+//        This function registers what user inserted and submitted with keyboard return key
+//        searchTextField.endEditing(true) dismisses keyboard after typing andpressing return
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+    }
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+//        Good for validation and checking if text is not empty screen etc...
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something..."
+            return false
+        }
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+//        To be triggered after editing ended
+//        Use searchTextField.text for searching the weather. Then reset search to empty string.
+        searchTextField.text = ""
+    }
 }
 
