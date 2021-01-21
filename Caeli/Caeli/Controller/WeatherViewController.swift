6664be34b5b6a5071387b12fd2449ab66b85db93
Kieran O'Donnell <kieran.j.odonnell@gmail.com>
2021-01-21 18:13:03 +1100

    removed comments

--- Caeli/Caeli/Controller/WeatherViewController.swift
+++ Caeli/Caeli/Controller/WeatherViewController.swift
@@ -14,7 +14,6 @@ class WeatherViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-// Add section for weather app control
-Insert UI elements 
+
 }
 
