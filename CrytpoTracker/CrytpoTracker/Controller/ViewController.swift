5faa7a82a3249f280017ea5ebaadb4154b452620
Kieran O'Donnell <kieran.j.odonnell@gmail.com>
2021-01-27 11:14:40 +1100

    defined view

--- CrytpoTracker/CrytpoTracker/Controller/ViewController.swift
+++ CrytpoTracker/CrytpoTracker/Controller/ViewController.swift
@@ -8,11 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1 // Only want 1 column in picker
+    }
+    let coinManager = CoinManager()
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count // Number of rows from currencies insinde array
+    }
+    
 
+    
+    @IBOutlet weak var bitCoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    
+    @IBOutlet weak var currencyPicker: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        currencyPicker.dataSource = self
     }
 
 
