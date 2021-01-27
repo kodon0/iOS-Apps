d012929c7d15ed2716dd995c3290fd7064a8f456
Kieran O'Donnell <kieran.j.odonnell@gmail.com>
2021-01-27 12:08:44 +1100

    added ui picker and delegate

--- CrytpoTracker/CrytpoTracker/Controller/ViewController.swift
+++ CrytpoTracker/CrytpoTracker/Controller/ViewController.swift
@@ -8,16 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController,UIPickerViewDataSource {
+class ViewController: UIViewController,UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    let coinManager = CoinManager()
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1 // Only want 1 column in picker
     }
-    let coinManager = CoinManager()
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return coinManager.currencyArray.count // Number of rows from currencies insinde array
     }
-    
-
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(coinManager.currencyArray[row])
+        let selectedCurrency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: selectedCurrency)
+    }
     
     @IBOutlet weak var bitCoinLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
@@ -26,6 +33,7 @@ class ViewController: UIViewController,UIPickerViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         currencyPicker.dataSource = self
+        currencyPicker.delegate =  self
     }
 
 
