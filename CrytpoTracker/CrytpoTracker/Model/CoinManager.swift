0f2811b3fd0c35ef4e94d065dae5ce4fde48aa37
Kieran O'Donnell <kieran.j.odonnell@gmail.com>
2021-01-29 17:15:31 +1100

    update

--- CrytpoTracker/CrytpoTracker/Model/CoinManager.swift
+++ CrytpoTracker/CrytpoTracker/Model/CoinManager.swift
@@ -28,20 +28,17 @@ struct CoinManager {
     }
     
     func performRequest(with urlString: String){
-        //        Check if string is nil
-//        print(URL(string: urlString) ?? "Nil URL")
-//        print("*\(urlString)*")
-        //        1) Create URL
+
         if let url = URL(string: urlString){
-            //        2) Create URL session
+
             let session = URLSession(configuration: .default)
-            //        3) Give session a task
+
             let task = session.dataTask(with: url) { (data, response, error) in
                 if error != nil {
-                    //            If there is an error, print it in debug console
+
                     print(error!)
                     self.delegate?.didFailWithError(error: error!)
-                    //                    Pass error to delegate
+
                     return
                 }
 
@@ -52,7 +49,6 @@ struct CoinManager {
                 }
                 
             }
-                    //        4) Start task
             task.resume()
         }
     }
