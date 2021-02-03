2b34b5cb692a317fa673e7be1bd403bdd8c83439
Kieran O'Donnell <kieran.j.odonnell@gmail.com>
2021-02-03 12:12:25 +1100

    updated
    added error reporting for  registration and login

--- BaxChat/BaxChat/Controllers/LoginViewController.swift
+++ BaxChat/BaxChat/Controllers/LoginViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
-
-    @IBAction func loginPressed(_ sender: UIButton) {
+//    Define the show alert function
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
     
+    @IBAction func loginPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+        
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            if error != nil {
+                if let errCode = AuthErrorCode(rawValue: error!._code) {
+                //                switch statement to flip between different error codes https://firebase.google.com/docs/reference/swift/firebaseauth/api/reference/Enums/AuthErrorCode#/c:@E@FIRAuthErrorCode@FIRAuthErrorCodeInvalidCustomToken
+                    switch errCode {
+                        case .invalidEmail:
+                            print("Invalid email")
+                            self.showAlert(message: "Invalid email")
+                        case .emailAlreadyInUse:
+                            print("Email in use")
+                            self.showAlert(message: "Email in use")
+                        case .operationNotAllowed:
+                            print("Operation not allowed")
+                            self.showAlert(message: "Operation not allowed")
+                        case .invalidCustomToken:
+                            print("Invalid token")
+                            self.showAlert(message: "Invalid token")
+                        case .wrongPassword:
+                            print("Incorrect password")
+                            self.showAlert(message: "Incorrect password")
+                        case .userDisabled:
+                             print("User banned")
+                             self.showAlert(message: "User banned")
+                        case .userNotFound:
+                            print("User doesn't exist")
+                            self.showAlert(message: "User doesn't exist")
+                        default:
+                            print("Create User Error: \(error!)")
+                    }
+                }
+            } else {
+                self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                
+            }
+
+            }
+        }
+    }
 }
--- BaxChat/BaxChat/Controllers/RegisterViewController.swift
+++ BaxChat/BaxChat/Controllers/RegisterViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+//        Show an alert pop-up defined in showAlert function
+        if let email = emailTextfield.text, let password = passwordTextfield.text{
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let e = error {
+                print(e.localizedDescription)
+                self.showAlert(message: e.localizedDescription)
+                
+            } else {
+                // If no error, go to chat view controller via segue inside enclosure
+                self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+            }
+        }
+        }
     }
     
 }
+
 
