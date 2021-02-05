b4c824dcbe7fe1176512ff6fcbb8c227f40c6f70
Kieran O'Donnell <kieran.j.odonnell@gmail.com>
2021-02-05 14:05:26 +1100

    modified animation

--- BaxChat/BaxChat/Controllers/WelcomeViewController.swift
+++ BaxChat/BaxChat/Controllers/WelcomeViewController.swift
@@ -29,15 +29,15 @@ class WelcomeViewController: UIViewController {
         super.viewDidLoad()
         
 //       Animated text with incremental delivery using timer
-        titleLabel.text = ""
-        var charIndex = 0.0
-        let titleText = K.appName
-        for letter in titleText{
-            Timer.scheduledTimer(withTimeInterval: 0.2 * charIndex, repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
+//        titleLabel.text = ""
+//        var charIndex = 0.0
+//        let titleText = K.appName
+//        for letter in titleText{
+//            Timer.scheduledTimer(withTimeInterval: 0.2 * charIndex, repeats: false) { (timer) in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex += 1
+//        }
        
         titleLabel.text = K.appName
     }
