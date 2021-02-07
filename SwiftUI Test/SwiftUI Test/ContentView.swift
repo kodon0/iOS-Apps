b9ea1cd1204f82cac42d0ae9aeefa6fad31cccf8
Kieran O'Donnell <kieran.j.odonnell@gmail.com>
2021-02-08 10:12:38 +1100

    Started Swift UI test app

---
+++ SwiftUI Test/SwiftUI Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Test
+//
+//  Created by Kieran O'Donnell on 08/02/2021.
+//  Copyright © 2021 baxmanduppa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
