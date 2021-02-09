871e21d2e87b9ba1e0b8f011dba2653cfcba1ae0
Kieran O'Donnell <kieran.j.odonnell@gmail.com>
2021-02-10 10:54:04 +1100

    Created basic SwiftUI dice game

---
+++ DiceGame_SwiftUI/DiceGame_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DiceGame_SwiftUI
+//
+//  Created by Kieran O'Donnell on 10/02/2021.
+//  Copyright © 2021 baxmanduppa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
