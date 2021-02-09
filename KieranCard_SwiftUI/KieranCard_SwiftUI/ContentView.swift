46aad6602adf717aba2f547ab9dd79cbb1571ea5
Kieran O'Donnell <kieran.j.odonnell@gmail.com>
2021-02-09 11:38:12 +1100

    finished app

--- KieranCard_SwiftUI/KieranCard_SwiftUI/ContentView.swift
+++ KieranCard_SwiftUI/KieranCard_SwiftUI/ContentView.swift
@@ -16,15 +16,27 @@ struct ContentView: View {
             VStack {
                 
                 
-                Image(/*@START_MENU_TOKEN@*/ /*@PLACEHOLDER=Image Name@*/"Image Name"/*@END_MENU_TOKEN@*/)
-                Text("Kieran O'Donnell")
+                Image("2150BB22-68E2-4A96-9B4A-1A27750C89BC_1_201_a").resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150.0, height: 240.0)
+                    .clipShape(Rectangle())
+                    .overlay(Rectangle().stroke(Color.white, lineWidth: 5))
+                Text("Kieran O'D")
                     .font(Font.custom("Ubuntu-Medium", size: 30))
                     .bold()
                     .foregroundColor(.white)
                 Text("iOS Developer, Data Scientist")
                     .foregroundColor(.white)
                     .font(Font.custom("Ubuntu-Medium", size: 15))
+                Divider()
+                InfoView(text: "+123 456 789")
+                Image(systemName: "phone.down.circle")
+                .foregroundColor(.white)
+                InfoView(text: "email@email.com")
+                Image(systemName: "envelope")
+                .foregroundColor(.white)
             }
+                    
         }
     }
 }
@@ -32,5 +44,17 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct InfoView: View {
+    
+    let text: String
+    var body: some View {
+        RoundedRectangle(cornerRadius: 35)
+            .frame(height: 35)
+            .foregroundColor(.white)
+            .overlay(Text(text).font(Font.custom("Ubuntu-Medium", size: 20)))
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
