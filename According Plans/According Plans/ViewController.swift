4ee6e5deac2edd1ebe065c159605b67fec10b3d6
Kieran O'Donnell <kieran.j.odonnell@gmail.com>
2021-02-15 14:55:49 +1100

    Started According Plans
    A clear - to-do list type of clone project with data base usage and local access

---
+++ According Plans/According Plans/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  According Plans
+//
+//  Created by Kieran O'Donnell on 15/02/2021.
+//  Copyright © 2021 baxmanduppa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
