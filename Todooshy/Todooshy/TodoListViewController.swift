ca94049d0da5fc93718a2e4f1d61f1e996baeb0a
Kieran O'Donnell <kieran.j.odonnell@gmail.com>
2021-02-16 14:05:06 +1100

    Added tableview controller
    added methods for displaying cell in table view controller

---
+++ Todooshy/Todooshy/TodoListViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Todooshy
+//
+//  Created by Kieran O'Donnell on 16/02/2021.
+//  Copyright © 2021 baxmanduppa. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Cuddle Jessie", "Give Jessie a mlem", "Cook a nice dinner for Jessie"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+}
+
