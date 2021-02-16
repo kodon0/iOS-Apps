a9a6cc6fc1fcbe58b00b1342b2a2bc5c340c43e7
Kieran O'Donnell <kieran.j.odonnell@gmail.com>
2021-02-17 10:04:22 +1100

    refactored code

--- Todooshy/Todooshy/Controller/TodoListViewController.swift
+++ Todooshy/Todooshy/Controller/TodoListViewController.swift
@@ -22,7 +22,6 @@ class TodoListViewController: UITableViewController {
         
         let newItem = Item()
         newItem.title = "Mlem Jessie!"
-        newItem.done = true
         itemArray.append(newItem)
         
         let newItem2 = Item()
@@ -32,12 +31,11 @@ class TodoListViewController: UITableViewController {
         let newItem3 = Item()
         newItem3.title = "Peepee!"
         itemArray.append(newItem3)
-        
-        
-//
-//        if let items = defaults.array(forKey: "TodoListArray") as? [String]{
-//
-//        }
+
+
+       if let items = defaults.array(forKey: "TodoListArray") as? [Item]{
+        itemArray = items
+     }
     }
 
     // MARK: - TableView DataScource Methods
@@ -47,15 +45,14 @@ class TodoListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        print("cellFor Row at index path called")
+
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row].title
         
-        if itemArray[indexPath.row].done == true {
-            cell.accessoryType = .checkmark
-        } else {
-            cell.accessoryType = .none
-        }
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        
+//
+        cell.accessoryType = item.done ? .checkmark: .none
         return cell
     }
 
@@ -63,14 +60,11 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(itemArray[indexPath.row].title)
-        print(itemArray[indexPath.row].done)
+        print(!itemArray[indexPath.row].done)
         tableView.deselectRow(at: indexPath, animated: true)
         
-        if itemArray[indexPath.row].done == false {
-            itemArray[indexPath.row].done = true
-        } else {
-            itemArray[indexPath.row].done = false
-        }
+//        Setting done status to be opposite of what it is currently
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
         tableView.reloadData()
         
