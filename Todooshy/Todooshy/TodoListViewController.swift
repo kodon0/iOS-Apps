367fecc42788f76bb6172f8025203e643282a473
Kieran O'Donnell <kieran.j.odonnell@gmail.com>
2021-02-16 15:12:40 +1100

    Added new items
    added capacity for user to insetr new items to list

--- Todooshy/Todooshy/TodoListViewController.swift
+++ Todooshy/Todooshy/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Cuddle Jessie", "Give Jessie a mlem", "Cook a nice dinner for Jessie"]
+    var itemArray = ["Cuddle Jessie", "Give Jessie a mlem", "Cook a nice dinner for Jessie"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +28,43 @@ class TodoListViewController: UITableViewController {
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
     }
-}
 
     // MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+    }
+    
+    //MARK: - Add new list items - with pop-up alert!
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Insert a new Todooshy item?", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add new item", style: .default) { (action) in
+            // What heppens when user clicks Add Item button
+            print("Action success!")
+            
+            // Append new item to array
+            self.itemArray.append(textField.text!)
+            
+            // Reloads the data with new items
+            self.tableView.reloadData()
+        }
+        alert.addAction(action)
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Start typing away!"
+            textField = alertTextField
+        }
+        
+        present(alert, animated: true, completion: nil)
+    }
+}
